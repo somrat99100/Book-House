3d3546519aa6c9006cda8eb7c2494823cbb1584f
Md. Mizanur Rahman Somrat <somraat999@gmail.com>
2026-08-05 01:20:46 +0600

    Initialize Firebase in app.js

--- js/app.js
+++ js/app.js
@@ -334,3 +334,29 @@ function applyFilters() {
 
   displayBooks();
 }
+
+
+
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAzSlk-rvcQiOqI5pSqWAJXGpEvasCBo-s",
+  authDomain: "book-house-ee2d6.firebaseapp.com",
+  projectId: "book-house-ee2d6",
+  storageBucket: "book-house-ee2d6.firebasestorage.app",
+  messagingSenderId: "751755650288",
+  appId: "1:751755650288:web:c7c4bc87980dd9a7b40e35",
+  measurementId: "G-BBYXXNBPL9"
+};
+
+// Initialize Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+console.log('Firebase initialized successfully');
